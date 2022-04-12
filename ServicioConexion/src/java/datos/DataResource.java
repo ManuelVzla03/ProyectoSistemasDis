42f04de5d261033383b3301a4d6a811cdd2677dc
Orlando Camacho Gámez <92180857+orlandocamacho19@users.noreply.github.com>
2022-04-11 23:26:58 -0700

    Conexiones

---
+++ ServicioConexion/src/java/datos/DataResource.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/GenericResource.java to edit this template
+ */
+package datos;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author orlandocamacho
+ */
+@Path("data")
+public class DataResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of DataResource
+     */
+    public DataResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of datos.DataResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJson() {
+        String hola = "hola";
+        return Response.ok().entity(hola).build();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of DataResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+        
+    }
+}
